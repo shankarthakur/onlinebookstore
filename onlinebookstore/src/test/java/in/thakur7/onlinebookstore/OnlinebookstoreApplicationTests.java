f28575b3b44441c6900ba61ffffe28f5cca2fcc4
shankar thakur <shankarthaur07@gmail.com>
2020-11-10 09:13:35 +0530

    intial commit. created spring boot project and configure the application properties.

---
+++ onlinebookstore/src/test/java/in/thakur7/onlinebookstore/OnlinebookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package in.thakur7.onlinebookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinebookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
